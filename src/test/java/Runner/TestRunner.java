c8651663a16cdcc3a5b74e929e69d176854d90d0
CorreoArgentinoAutomation <86322429+CorreoArgentinoAutomation@users.noreply.github.com>
2024-02-22 20:23:55 -0300

    Update TestRunner.java

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -6,12 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        /* plugin = {"json:target/build/cucumber.json"}, */
+        plugin = {"json:target/build/cucumber.json"},
         stepNotifications = true,
         publish = true,
         features = {"src/test/java/features/search.feature"},
         glue = {"steps"},
-        plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" },
+       /*  plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" },  */
         monochrome = true,
         tags =  "@PRUEBA2" )
 public class TestRunner {
